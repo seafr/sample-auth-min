31bda09123b8de43f9bbcc0ed8dbd20051346ae6
Sea <seafr@users.noreply.github.com>
2017-08-30 20:14:30 +0400

    Add format and uniqueness validations for username. (Using regular expressions finally, see commit 3dd4b34

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
-  validates :username, presence: true, length: { minimum: 4 }
+  VALID_USERNAME = /\A[a-z][a-z\-\_]+[a-z]\z/i
+  validates :username, presence: true, length: { minimum: 4 },
+		       format: { with: VALID_USERNAME },
+		       uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 4 }
 end
