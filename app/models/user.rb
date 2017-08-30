c8593d847f023167f5317f7ba87368a27afa261e
Sea <seafr@users.noreply.github.com>
2017-08-30 19:35:53 +0400

    Add validations for User's attributes. Add partial in signup form to display errors when signup fails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { minimum: 4 }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 4 }
 end
