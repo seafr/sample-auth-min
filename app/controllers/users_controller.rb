453c77c3ae4b1fbefc0f7a3909980e7464fa715b
Sea <seafr@users.noreply.github.com>
2017-08-30 17:47:35 +0400

    Add password_digest attribute to User model. Create actions in Users controller to save and show user data. Edit user signup form for new user to enter and confirm password. Create view to show user data.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      # only allow some attributes to prevent mass assignment
+      params.require(:user).permit(:name, :username, :password,
+      				   :password_confirmation)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830115401) do
+ActiveRecord::Schema.define(version: 20170830131925) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
