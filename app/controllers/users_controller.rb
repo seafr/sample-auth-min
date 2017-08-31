a1debdaa108b0564acafc9babc747985445c1a61
Sea <seafr@users.noreply.github.com>
2017-09-01 00:21:06 +0400

    Add flash message on successful signup and code to display it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Your account has been created."
       redirect_to @user
     else
       render 'new'
