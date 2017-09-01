9f7c3ffc2a90d65279b9e057b04eefc048172bda
Sea <seafr@users.noreply.github.com>
2017-09-01 22:05:46 +0400

    Add ability to log user in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Login successful!"
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
 end
