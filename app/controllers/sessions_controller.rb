f9521cabb96006fb4e86cb40188c1a322211617d
Sea <seafr@users.noreply.github.com>
2017-09-05 22:05:30 +0400

    Refactor code that logs user in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+      log_in user
       flash[:success] = "Login successful!"
       redirect_to user
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      log_in @user
       flash[:success] = "Your account has been created."
       redirect_to @user
     else
