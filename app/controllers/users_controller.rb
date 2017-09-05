416dc5e33ede06db3fcd54cadd02be9dd195ee82
Sea <seafr@users.noreply.github.com>
2017-09-05 21:46:46 +0400

    Automatically log in user after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Your account has been created."
       redirect_to @user
     else
