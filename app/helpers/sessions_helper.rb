500906d21481f06a79ab89f7d54d192c0fc8bcd1
Sea <seafr@users.noreply.github.com>
2017-09-05 22:32:07 +0400

    Refactor code for user login status and currently logged in user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  def user_logged_in?
+    !session[:user_id].nil?
+  end
+  def current_user
+    @current_user ||= User.find_by id: session[:user_id]
+  end
 end
