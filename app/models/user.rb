2b19d80370ec53181fc8f73c6f61b9a61b7b3d70
Sea <seafr@users.noreply.github.com>
2017-09-08 16:25:10 +0400

    Edit user model to accept numbers in usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.username = username.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_USERNAME = /\A[a-z][a-z\-\_]+[a-z]\z/i
+  VALID_USERNAME = /\A[a-z][a-z0-9\-\_]+[a-z0-9]\z/i
   validates :username, presence: true, length: { minimum: 4 },
 		       format: { with: VALID_USERNAME },
 		       uniqueness: { case_sensitive: false }
