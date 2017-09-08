b4fba8dba6360f99c75a1a501ffc912f87a74ef8
Sea <seafr@users.noreply.github.com>
2017-09-08 16:22:37 +0400

    Add 'edit' and 'update' actions in Users controller. Create template for showing a user's settings. Add link to 'Settings' in the header.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Your account has been created."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account has been updated."
       redirect_to @user
     else
       render 'new'
