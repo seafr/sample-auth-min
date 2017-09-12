8b8226c983f6b8265c97abbd648e98fff1887366
Sea <seafr@users.noreply.github.com>
2017-09-12 19:06:59 +0400

    Allow a user to update their settings without changing their password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
 		       format: { with: VALID_USERNAME },
 		       uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 4 }
+  validates :password, presence: true, length: { minimum: 4 },
+  		       allow_nil: true
 end
